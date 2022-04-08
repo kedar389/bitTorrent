0ce782c79ccd88bfa942077e93f2229bfc6a2255
kedar389 <kedar389@gmail.com>
2022-04-08 02:20:39 +0200

    Formatted client

--- client.py
+++ client.py
@@ -2,13 +2,6 @@ import asyncio
 from torrent_client import TorrentClient
 
 
-
 def main():
     loop = asyncio.get_event_loop()
     torrent_client = TorrentClient('C:\\Users\\RADEK_RYZEN\\Downloads\\ubuntu-21.10-desktop-amd64.iso.torrent')
-
-
-
-
-
-
