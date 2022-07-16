cf70f1e6ce21abb8ef2a7c770b42d23c58ef2936
kedar389 <kedar389@gmail.com>
2022-07-16 02:52:23 +0200

    added starting client

--- client.py
+++ client.py
@@ -1,12 +1,20 @@
 import asyncio
+from torrent import Torrent
 from torrent_client import TorrentClient
+from tracker import Tracker
+import logging
+
+# TODO Figure out how to accept connections from peers when you are finished with downloading, or if tracker gives u them
+# TODO how to seed when you are finished , if you should break in torrent_client .
 
 
-#TODO Figure out how to accept connections from peers when you are finished with downloading, or if tracker gives u them
-#TODO how to seed when you are finished , if you should break in torrent_client .
 
-
-def main():
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
     torrent_client = TorrentClient('C:\\Users\\RADEK_RYZEN\\Downloads\\ubuntu-21.10-desktop-amd64.iso.torrent')
     task = loop.create_task(torrent_client.start())
+
+    loop.run_until_complete(task)
+
+
